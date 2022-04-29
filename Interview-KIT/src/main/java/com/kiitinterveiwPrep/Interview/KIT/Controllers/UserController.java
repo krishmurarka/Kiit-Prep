311141d0a7e766553cf68d1999ac9003151c43ca
krishmurarka <murarka.krish@gmail.com>
2022-04-29 11:05:14 +0530

    Validated User Data using Bean Validator

--- Interview-KIT/src/main/java/com/kiitinterveiwPrep/Interview/KIT/Controllers/UserController.java
+++ Interview-KIT/src/main/java/com/kiitinterveiwPrep/Interview/KIT/Controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -21,14 +22,14 @@ public class UserController {
     private UserService userService;
     // POST - Create User
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
          UserDto createUserDto =this.userService.createUser(userDto);
          return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
 
     // PUT - Update User
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Integer userId){
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto,@PathVariable("userId") Integer userId){
         UserDto updatedUser = this.userService.updateUser(userDto,userId);
 
         return ResponseEntity.ok(updatedUser);
